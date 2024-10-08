3ea6a7cd1658394a1da7ace870ffd15ac1c68cec
Mateus <mateuscarvalhon2@gmail.com>
2024-10-07 22:35:35 -0300

    Favicon and Preload

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+function loading(){
+    if (document.readyState === "complete") {
+        setTimeout(function() {
+            document.getElementById('code-loader').style.display = 'none'
+            document.getElementById('max').style.display = 'block'
+        }, 2000);
+        
+
+        }
+}
